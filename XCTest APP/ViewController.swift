f9b423072277ffb688f52ac920d50b5ca1192eca
pmatasnjov <p.matasnjov@cft.ru>
2024-01-13 13:32:42 +0700

    Initial Commit

---
+++ XCTest APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCTest APP
+//
+//  Created by Маташнёв Павел Анатольевич on 13.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
