cb0c9d211ff7b5e35651a551f06b020f3b34cc73
pmatasnjov <p.matasnjov@cft.ru>
2024-01-13 21:40:20 +0700

    добавила кнопку, меняющую текст

--- XCTest APP/ViewController.swift
+++ XCTest APP/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var greetingView: UITextView!
+	@IBOutlet weak var okayButton: UIButton!
+	@IBOutlet weak var nextButton: UIButton!
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		hideNextButton()
 		// Do any additional setup after loading the view.
 	}
+	@IBAction func changeGreeting() {
+		greetingView.text = "Замечательно!"
+		okayButton.isHidden = true
+		nextButton.isHidden = false
+		
+	}
 
-
+	func hideNextButton() {
+		nextButton.isHidden = true
+	}
 }
 
