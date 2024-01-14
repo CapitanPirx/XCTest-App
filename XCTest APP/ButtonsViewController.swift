93f81da751de07205e3f353a82a6283fea63e81b
pmatasnjov <p.matasnjov@cft.ru>
2024-01-14 13:55:22 +0700

    Добавил экран меню, кнопки и экран с текстовыми полями, пока без логики

---
+++ XCTest APP/ButtonsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsViewController.swift
+//  XCTest APP
+//
+//  Created by Маташнёв Павел Анатольевич on 14.01.2024.
+//
+
+import UIKit
+
+class ButtonsViewController: UIViewController {
+
+	@IBOutlet weak var textFieldButton: UIButton!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
