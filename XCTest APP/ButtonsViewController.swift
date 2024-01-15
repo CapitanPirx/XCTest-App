98a5ac47ad39a41231e38a68e5c40d731f401f0d
pmatasnjov <p.matasnjov@cft.ru>
2024-01-15 22:03:43 +0700

    добавил алерт и боттомшит

--- XCTest APP/ButtonsViewController.swift
+++ XCTest APP/ButtonsViewController.swift
@@ -10,7 +10,37 @@ import UIKit
 class ButtonsViewController: UIViewController {
 
 	@IBOutlet weak var textFieldButton: UIButton!
+
 	override func viewDidLoad() {
         super.viewDidLoad()
     }
+	@IBAction func alert(_ sender: UIButton, forEvent event: UIEvent) {
+		showAlert(title: "Вот тебе алерт!", message: "А боттомшит не хочешь?")
+	}
+	
+}
+
+extension ButtonsViewController {
+	
+	private func showAlert(title: String, message: String) {
+		
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		
+		let okButton = UIAlertAction(title: "Нет!", style: .default)
+		alert.addAction(okButton)
+		
+		alert.addAction(UIAlertAction(title: "Пох, давай", style: .default, handler: { action in self.showBottomSheet(title: "Получи", message: "Распишись")
+			
+		}))
+		present(alert, animated: true)
+	}
+
+	private func showBottomSheet(title: String, message: String) {
+		
+		let bottomsheet = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+
+		let okButton = UIAlertAction(title: "Прикольно", style: .default)
+		bottomsheet.addAction(okButton)
+		present(bottomsheet, animated: true)
+	}
 }
