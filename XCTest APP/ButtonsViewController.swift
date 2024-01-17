3851ef65e917e89c094c4f832d09fab6f73837df
pmatasnjov <p.matasnjov@cft.ru>
2024-01-17 22:05:14 +0700

    Добавил уведомление

--- XCTest APP/ButtonsViewController.swift
+++ XCTest APP/ButtonsViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ButtonsViewController: UIViewController {
+	
+	let notificationCenter = UNUserNotificationCenter.current()
 
 	@IBOutlet weak var textFieldButton: UIButton!
 
@@ -17,7 +20,9 @@ class ButtonsViewController: UIViewController {
 	@IBAction func alert(_ sender: UIButton, forEvent event: UIEvent) {
 		showAlert(title: "Вот тебе алерт!", message: "А боттомшит не хочешь?")
 	}
-	
+	@IBAction func notification(_ sender: UIButton) {
+		sendNotifications()
+	}
 }
 
 extension ButtonsViewController {
@@ -30,7 +35,6 @@ extension ButtonsViewController {
 		alert.addAction(okButton)
 		
 		alert.addAction(UIAlertAction(title: "Пох, давай", style: .default, handler: { action in self.showBottomSheet(title: "Получи", message: "На самом деле, это тоже алерт, просто в другом виде")
-			
 		}))
 		present(alert, animated: true)
 	}
@@ -42,6 +46,21 @@ extension ButtonsViewController {
 		let okButton = UIAlertAction(title: "Прикольно", style: .default)
 		bottomsheet.addAction(okButton)
 		present(bottomsheet, animated: true)
+	}
+	
+	private func sendNotifications() {
+		
+		let content = UNMutableNotificationContent()
+		content.title = "Уведомляю!"
+		content.body = "Вообще это не push-уведомление, push только через сервер, а это локальное"
+		content.sound = UNNotificationSound.default
+		
+		let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.01, repeats: false)
+		
+		let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+		notificationCenter.add(request){ (error) in
+			print(error?.localizedDescription)
+		}
 	}
 }
 // TODO: вынести все строки в отдельный файл
